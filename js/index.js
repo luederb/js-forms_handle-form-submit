155836b3bb11877ef6af0b7a8275d74ce3ab7f91
Lueder Budde <lueder.budde@gmail.com>
2023-10-04 15:35:39 +0100

    solution to the challenge

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formElements = event.target.elements;
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  const ageBadnessSum =
+    parseInt(formElements.age.value) + parseInt(formElements.badness.value);
+  console.log(data);
+  console.log(
+    `The age-badness-sum of ${formElements.firstName.value} is ${ageBadnessSum}`
+  );
+  console.log(event.target.elements);
+  formElements.firstName.focus();
+  event.target.reset();
+});
